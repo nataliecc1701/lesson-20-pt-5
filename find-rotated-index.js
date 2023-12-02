9534850a00cb92608f8d9d5fd1e294f9a70b0b8a
Natalie Chapman <natalie.c.chapman628@gmail.com>
2023-12-02 00:59:01 -0800

    find rotated index works

--- find-rotated-index.js
+++ find-rotated-index.js
@@ -1,5 +1,25 @@
-function findRotatedIndex() {
- 
+function findRotatedIndex(arr, tgt) {
+    let minPointer = -1;
+    let maxPointer = arr.length;
+    let i;
+    
+    while (maxPointer - minPointer > 1) {
+        i = Math.floor((minPointer + maxPointer) / 2);
+        if (arr[i] === tgt) return i
+        if (tgt >= arr[0]) {
+            if (arr[i] > tgt || arr[i] < arr[0]) {
+                maxPointer = i;
+            }
+            else minPointer = i;
+        }
+        else {
+            if (arr[i] < tgt || arr[i] > arr[0]) {
+                minPointer = i;
+            }
+            else maxPointer = i;
+        }
+    }
+    return -1
 }
 
 module.exports = findRotatedIndex
