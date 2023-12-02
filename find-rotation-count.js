6eb8238331458ba9607fd0816224d78e6c9a8b17
Natalie Chapman <natalie.c.chapman628@gmail.com>
2023-12-02 00:40:41 -0800

    removed debug code

--- find-rotation-count.js
+++ find-rotation-count.js
@@ -2,23 +2,19 @@ function findRotationCount(arr) {
   let minPointer = -1;
   let maxPointer = arr.length;
   let i;
-  let count=0
   
   if (arr[arr.length-1] > arr[0]) {
     return 0
   }
   
   while (maxPointer - minPointer > 1) {
-    if (++count >= 10000) {break}
     i = Math.floor((minPointer + maxPointer) / 2);
     
     if (arr[i] < arr[0]) {
         maxPointer = i;
-        console.log(`max ${i}`)
     }
     else {
         minPointer = i;
-        console.log(`min ${i}`)
     }
   }
   return maxPointer
