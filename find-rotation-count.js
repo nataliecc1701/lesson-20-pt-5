80353800fa28a7de5e9e768bee38b0904d83d40d
Natalie Chapman <natalie.c.chapman628@gmail.com>
2023-12-02 00:40:23 -0800

    rotation count works

--- find-rotation-count.js
+++ find-rotation-count.js
@@ -1,5 +1,27 @@
-function findRotationCount() {
+function findRotationCount(arr) {
+  let minPointer = -1;
+  let maxPointer = arr.length;
+  let i;
+  let count=0
   
+  if (arr[arr.length-1] > arr[0]) {
+    return 0
+  }
+  
+  while (maxPointer - minPointer > 1) {
+    if (++count >= 10000) {break}
+    i = Math.floor((minPointer + maxPointer) / 2);
+    
+    if (arr[i] < arr[0]) {
+        maxPointer = i;
+        console.log(`max ${i}`)
+    }
+    else {
+        minPointer = i;
+        console.log(`min ${i}`)
+    }
+  }
+  return maxPointer
 }
 
 module.exports = findRotationCount
