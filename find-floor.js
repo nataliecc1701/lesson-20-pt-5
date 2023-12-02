9df5a7a8e617ea6525f814d212fd0680d8be40ae
Natalie Chapman <natalie.c.chapman628@gmail.com>
2023-12-02 01:05:25 -0800

    function working

--- find-floor.js
+++ find-floor.js
@@ -1,5 +1,20 @@
-function findFloor() {
-  
+function findFloor(arr, tgt) {
+    let minPointer = -1;
+    let maxPointer = arr.length;
+    let i;
+    
+    while (maxPointer - minPointer > 1) {
+        i = Math.floor((minPointer + maxPointer) / 2);
+        if (arr[i] === tgt) return arr[i]
+        else if (arr[i] > tgt) {
+            maxPointer = i
+        }
+        else if(arr[i] < tgt) {
+            minPointer = i
+        }
+    }
+    if (minPointer > -1) return arr[minPointer]
+    else return -1
 }
 
 module.exports = findFloor
